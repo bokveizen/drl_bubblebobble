1097768feb450797c8d0d71bc9bb92dd0abd72e4
BoqVezen <boqvezen97@kaist.ac.kr>
2020-06-29 15:54:36 +0900

    saved acts updated

--- MCTS_res_process.py
+++ MCTS_res_process.py
@@ -1,4 +1,14 @@
-from pathlib import Path
+import os
 
-res_dir = Path('saved_acts_MCTS_0628')
 
+def find_best_acts(path, level, aux):
+    level_acts_files = [f for f in os.listdir(path) if 'lvl{}to{}_'.format(level, level + 1) in f]
+    if level_acts_files:
+        return os.path.join(path, max(level_acts_files, key=lambda f: int(f[f.find('score') + 5:f.find('acts') - 1])))
+    return '{}/lvl{}_acts.pickle'.format(aux, level)
+
+
+auxiliary_dir = 'saved_acts_one_level_only'
+acts_dir = 'saved_acts_MCTS'
+for lvl in range(1, 100):
+    print(find_best_acts(acts_dir, lvl, auxiliary_dir))
